817fc3d1edf2cd9f349b12d51fc67816c04d7193
aaronms1 <amsanon1@proton.me>
2025-01-11 02:23:03 -0500

    added gpu processing classes

---
+++ src/main/java/org/dacss/projectinitai/loader/DynamicModelLoaderKernel.java
@@ -0,0 +1,49 @@
+package org.dacss.projectinitai.loader;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import uk.ac.manchester.tornado.api.*;
+import uk.ac.manchester.tornado.api.enums.DataTransferMode;
+import uk.ac.manchester.tornado.api.exceptions.TornadoExecutionPlanException;
+
+import java.io.IOException;
+
+import static org.dacss.projectinitai.loader.ModelDirectoryHandler.loadModel;
+
+@Slf4j
+@Component
+public class DynamicModelLoaderKernel {
+
+    public DynamicModelLoaderKernel() {
+    }
+
+    private byte[] loadModelWithKernel(String modelPath) throws IOException {
+        try {
+            byte[] model = loadModel(modelPath);
+        } catch (IOException e) {
+            log.error("Error loading model: {}", e.getMessage());
+        }
+        byte[] modelData = loadModel(modelPath);
+
+        TaskGraph taskGraph = new TaskGraph("s0")
+                .transferToDevice(DataTransferMode.FIRST_EXECUTION, modelData)
+                .task("loadModel", () -> {
+                    KernelContext context = new KernelContext();
+                    int idx = context.globalIdx;
+                    if (idx < modelData.length) {
+                        //FIXME: ASAP this should be replaced with a real model loading process
+                        modelData[idx] = (byte) (modelData[idx] + 1);
+                    }
+                })
+                .transferToHost(DataTransferMode.EVERY_EXECUTION, modelData);
+
+        try (TornadoExecutionPlan executionPlan = new TornadoExecutionPlan()) {
+            executionPlan.getDevice(0).getAvailableProcessors();
+            executionPlan.withDynamicReconfiguration(Policy.PERFORMANCE, DRMode.PARALLEL).execute();
+        } catch (TornadoExecutionPlanException e) {
+            log.error("Error executing Tornado plan: {}", e.getMessage());
+        }
+
+        return modelData;
+    }
+}
--- src/main/java/org/dacss/projectinitai/loader/LLMProcessorComp.java
+++ src/main/java/org/dacss/projectinitai/loader/LLMProcessorComp.java
@@ -3,53 +3,43 @@ package org.dacss.projectinitai.loader;
 import lombok.extern.slf4j.Slf4j;
 import org.dacss.projectinitai.components.ContextualAdviserComp;
 import org.dacss.projectinitai.components.ProcessorFactoryComp;
+import org.dacss.projectinitai.enums.MessageType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class LLMProcessorComp {
-    private byte[] model;
+    private final DynamicModelLoaderKernel dynamicModelLoaderKernel;
+    private final ParallelizedModelLoader parallelizedModelLoader;
     private final ContextualAdviserComp<String> contextualAdviserComp;
     private final ProcessorFactoryComp processorFactoryComp;
 
     @Autowired
-    public LLMProcessorComp(ContextualAdviserComp<String> contextualAdviserComp, ProcessorFactoryComp processorFactoryComp) {
+    public LLMProcessorComp(DynamicModelLoaderKernel dynamicModelLoaderKernel, ParallelizedModelLoader parallelizedModelLoader,
+                            ContextualAdviserComp<String> contextualAdviserComp,
+                            ProcessorFactoryComp processorFactoryComp) {
+        this.dynamicModelLoaderKernel = dynamicModelLoaderKernel;
+        this.parallelizedModelLoader = parallelizedModelLoader;
         this.contextualAdviserComp = contextualAdviserComp;
         this.processorFactoryComp = processorFactoryComp;
-        try {
-            String modelPath = "path/to/your/model.blob";
-            String expectedChecksum = "your_expected_sha256_checksum";
-            if (ChecksumVerifier.verifyChecksum(modelPath, expectedChecksum)) {
-                this.model = ModelLoader.loadModel(modelPath);
-                // Initialize your model here
-            } else {
-                log.error("Checksum verification failed");
-//                Notification.show("Checksum verification failed");
-            }
-        } catch (Exception e) {
-            log.error("Failed to load the model", e);
-//            Notification.show("Failed to load the model: " + e.getMessage());
-        }
     }
 
     public void process(String input) {
-        // Use the loaded model to process the input
+        // Use the loaded model from DynamicModelLoaderKernel to process the input
+        byte[] model = dynamicModelLoaderKernel.getModel();
+        // Process the input using the model
     }
 
     public void integrateWithContextualAdviser(String input) {
+        // Use ParallelizedModelLoader to handle IO looping operations
+        parallelizedModelLoader.process(input);
         contextualAdviserComp.processUserInput(input);
     }
 
     public void integrateWithProcessorFactory(String input) {
-        processorFactoryComp.getStringProcessor(input);
-    }
-
-    public void integrateWithContextualAdviserComp(String input) {
-        contextualAdviserComp.processUserInput(input);
-    }
-
-    public void integrateWithProcessorFactoryComp(String input) {
-        processorFactoryComp.getStringProcessor(input);
+        // Use ParallelizedModelLoader to handle IO looping operations
+        parallelizedModelLoader.process(input);
+        processorFactoryComp.getStringProcessor(MessageType.valueOf(input));
     }
 }
